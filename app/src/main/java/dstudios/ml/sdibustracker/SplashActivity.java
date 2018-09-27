822084ded9f36b9437e97801cb35647b669db3be
imprashanthv <vissaprashanth@gmail.com>
2018-09-27 15:28:17 +0530

    Initial commit

---
+++ app/src/main/java/dstudios/ml/sdibustracker/SplashActivity.java
@@ -0,0 +1,28 @@
+package dstudios.ml.sdibustracker;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(),FirebaseActivity.class);
+                startActivity(intent);
+            }
+        },1900);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+    }
+}
